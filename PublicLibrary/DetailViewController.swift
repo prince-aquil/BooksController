a883750e4c74a5ef6068c254a4a294d3a68d2a74
Alberto-Vega <el_vega@live.com>
2015-07-08 22:05:08 -0700

    Changed labels for text fields and fixed auto layout on DetailViewControlle

--- PublicLibrary/DetailViewController.swift
+++ PublicLibrary/DetailViewController.swift
@@ -9,23 +9,24 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-  @IBOutlet weak var bookTitleLabel: UILabel!
-  @IBOutlet weak var bookAuthorLabel: UILabel!
-  @IBOutlet weak var bookPublisherLabel: UILabel!
-  @IBOutlet weak var bookPagesLabel: UILabel!
-  @IBOutlet weak var bookISBNLabel: UILabel!
+  
+  @IBOutlet weak var bookTitleTextField: UITextField!
+  @IBOutlet weak var bookAuthorTextField: UITextField!
+  @IBOutlet weak var bookPublisherTextField: UITextField!
+  @IBOutlet weak var bookPagesTextField: UITextField!
+  @IBOutlet weak var bookISBNTextField: UITextField!
   
   var selectedBook: Book!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    bookTitleLabel.text = selectedBook.title
-    bookAuthorLabel.text = selectedBook.author
-    bookPublisherLabel.text = selectedBook.publisher
+    bookTitleTextField.text = selectedBook.title
+    bookAuthorTextField.text = selectedBook.author
+    bookPublisherTextField.text = selectedBook.publisher
     if let bookPages = selectedBook.pages {
-      bookPagesLabel.text = String(stringInterpolationSegment: bookPages)
+      bookPagesTextField.text = String(stringInterpolationSegment: bookPages)
       if let bookISBN = selectedBook.iSBN {
-        bookISBNLabel.text = String(stringInterpolationSegment: bookISBN)
+        bookISBNTextField.text = String(stringInterpolationSegment: bookISBN)
       }
     }
   }
@@ -34,6 +35,46 @@ class DetailViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  private func setupTextFields() {
+    
+    self.bookTitleTextField.tag = 0
+    self.bookAuthorTextField.tag = 1
+    self.bookPublisherTextField.tag = 2
+    self.bookPagesTextField.tag = 3
+    self.bookISBNTextField.tag = 4
+
+    self.bookTitleTextField.text = self.selectedBook.title
+    self.bookAuthorTextField.text = self.selectedBook.title
+    self.bookPublisherTextField.text = self.selectedBook.title
+    self.bookPagesTextField.text = self.selectedBook.title
+    self.bookISBNTextField.text = self.selectedBook.title
+
+    
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    
+    textField.resignFirstResponder()
+    return false
+  }
+  
+  func textFieldDidEndEditing(textField: UITextField) {
+    
+    switch textField.tag {
+    case 0:
+      textField.text = selectedBook.title
+    case 1:
+      textField.text = self.selectedBook.author
+    case 2:
+      textField.text = self.selectedBook.publisher
+    case 3:
+      textField.text = String(stringInterpolationSegment: self.selectedBook.pages)
+    default:
+      textField.text = String(stringInterpolationSegment:self.selectedBook.iSBN)
+    }
+  }
+
   
   /*
   // MARK: - Navigation
